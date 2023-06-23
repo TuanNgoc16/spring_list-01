9dc3315c0070807e28585671f289d897f6549e70
Tuấn Ngọc <103101440+TuanNgoc16@users.noreply.github.com>
2023-06-23 14:10:33 +0700

    List Spring

---
+++ _spring_demo3/src/main/java/com/example/_spring_demo3/controller/EmployeeController.java
@@ -0,0 +1,39 @@
+package com.example._spring_demo3.controller;
+
+import com.example._spring_demo3.model.Employee;
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@SpringBootApplication
+@RequestMapping("/employees")
+
+public class EmployeeController {
+    private List<Employee> theEmployees;
+
+    @PostConstruct
+    private void loadData(){
+        Employee emp1 = new Employee(1,"tuan","ngoc", "abc@gmail.com");
+        Employee emp2 = new Employee(2,"tuan","ngoc", "abc@gmail.com");
+        Employee emp3 = new Employee(3,"tuan","ngoc", "abc@gmail.com");
+
+        theEmployees = new ArrayList<>();
+
+        theEmployees.add(emp1);
+        theEmployees.add(emp2);
+        theEmployees.add(emp3);
+
+    }
+    @GetMapping("/list")
+    public String listEmployees(Model theModel){
+        theModel.addAttribute("employees", theEmployees);
+        return "list_employees";
+    }
+}
